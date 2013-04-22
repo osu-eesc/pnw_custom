06def299759c44828ec68ce43190f8eddaadccda
Bryan Mayjor <bmayjor@gmail.com>
2013-04-22 09:39:53 -0700

    removed page top from module

--- pnw_custom.js
+++ pnw_custom.js
@@ -1,14 +1,40 @@
-(function ($){
-  $(document).ready(function() {
-    /* prepend menu icon */
-    $('#block-superfish-1').prepend('<div id="menu-icon">Menu</div>');
+// (function ($){
+//   $(document).ready(function() {
+//     /* prepend menu icon */
+//     $('#block-superfish-2').prepend('<button id="menu-icon" class="nav-button"></button>');
     
-    /* toggle nav */
-    $('#menu-icon').on('click', function(){
-        $('#superfish-1').slideToggle();
-        $(this).toggleClass("active");
-    });
+//     /* toggle nav */
+//     $('#menu-icon').on('click', function(){
+//         $('#superfish-2').slideToggle();
+//         $(this).toggleClass("active");
+//     });
 
-  });
+//   });
  
+// })(jQuery);
+
+// (function ($){
+// 	$(document).ready(function(){
+// 	/* prepend menu icon */
+//   $('#block-superfish-2').prepend('<button id="menu-icon" class="nav-button"></button>');
+	
+// 		$(".nav-button").click(function () {
+// 		$(".nav-button,.sf-menu").toggleClass("open");
+// 		}); 
+// 	});  
+// })(jQuery);
+
+(function ($){
+	$(document).ready(function(){
+		/* prepend menu icon */
+  	$('header').prepend('<button id="menu-icon" class="nav-button"></button>');
+  	/*  replace <nonlink> element with anchor */
+  	$('li.menu-item-480 span').replaceWith('<a title="Browse handbook contents" href="#">Contents</a>');
+  	$('#main-content ul.links').after('<div class="top-anchor"><a title="Top of page" href="#page">Back to top</a></div>');
+  	
+	
+		$(".nav-button").click(function () {
+		$(".nav-button,.menu").toggleClass("open");
+		}); 
+	});  
 })(jQuery);
